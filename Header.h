8a3702fee0eb7775105c522e7845f793ec279e20
collett31 <charltoncollett1@gmail.com>
2019-02-28 22:46:53 -0600

    Add files via upload

--- Header.h
+++ Header.h
@@ -8,8 +8,6 @@
 
 namespace COMPILERPROGRAM
 {
-
-
 class Compiler
 {
 	public:
@@ -20,7 +18,9 @@ class Compiler
 			for (int i = 0; i < 2; i++) {
 				for (int j = 0; j < 4;j++){
 					table[i][j] = "unassigned";
+					std::cout << table[i][j] << " ";
 				}
+				std::cout << std::endl;
 			}
 		}
 		
@@ -54,7 +54,6 @@ class Compiler
 		void showerror()
 		{
 			int i;
-
 			for (i = 0; i < errorcount; i = i + 1)
 				std::cout << error[i] << std::endl;
 		}
@@ -68,11 +67,25 @@ class Compiler
 			return code[i];
 		}
 		void run() {
+			errorcount = 0;
 			for (int i = 1; i < size - 1; i++) {
-				if (code[i].substr(0, 7) == "display" && code[i].size()>11) display_string(code[i]);
-				if (code[i].substr(0, 7) == "display" && code[i].size() == 10) { display_int(code[i]); }
-				if (code[i].substr(0, 5) == "input") { int_assign(code[i]); }
-				if (code[i][0] == 'A' || code[i][0] == 'B' || code[i][0] == 'C') { calculate(code[i]); std::cout << tempVar << std::endl; }
+				if (code[i].substr(0, 7) == "display" && code[i].size() > 11) {
+					display_string(code[i]);
+				}
+				if (code[i].substr(0, 7) == "display" && code[i].size() == 10) { 
+					if (!display_int(code[i])) {
+						showerror();
+					}
+				}
+				if (code[i].substr(0, 5) == "input") { 
+					if (!int_assign(code[i]))
+						showerror();
+				}
+				if (code[i][0] == 'A' || code[i][0] == 'B' || code[i][0] == 'C') { 
+					if(!calculate(code[i]))
+						showerror(); 
+				//	std::cout << tempVar << std::endl; 
+				}
 				//std::cout << " here1" << std::endl;;
 			}
 		}
@@ -85,8 +98,8 @@ class Compiler
 		std::string table[2][4];
 		int total, tablecount=0;
 		int temp=0, temp1=0, tempVar;
-		
-			
+		bool updated=false;
+
 		void statementcompile()
 		{
 			int i;
@@ -98,27 +111,11 @@ class Compiler
 					{
 						error[errorcount] = "Line # " + std::to_string(i + 1) + ": Incomplete display statement";
 						errorcount = errorcount + 1;// add to the error count once an error is made
-
 					}
-					if (code[i][code[i].length() - 1] != ';')
-						//if (code[i].back() != ';') // check whether it ends correctly
-						//if (code[i].substr(code[i].;ength(),1) != ';')
-					{
+					if (code[i][code[i].length() - 1] != ';'){
 						error[errorcount] = "Line # " + std::to_string(i + 1) + ": Incomplete display statement, ';' missing";
 						errorcount = errorcount + 1;// add to the error count once an error is made
-
-					}
-					//if (code[i].substr(code[i].length() - 2, 1) != "\"")
-					//{
-					//	error[errorcount] = "Line # " + std::to_string(i + 1) + ": Incomplete display statement, ';' missing";
-					//	errorcount = errorcount + 1;// add to the error count once an error is made
-
-					//}
-					//if (code[i].substr(8, 1) != "\"") {
-						//error[errorcount] = "Line # " + std::to_string(i + 1) + ": Incomplete display statement, \" missing";
-						//errorcount = errorcount + 1;// add to the error count once an error is made
-
-					//}
+					}	
 				}
 				 if (code[i].substr(0, 5) == "input") {
 					if (code[i].length() < 7) { // check whether there is actually something to input based on the length
@@ -132,94 +129,65 @@ class Compiler
 						 errorcount = errorcount + 1;// add to the error count once an error is mad
 					 }
 				 }
-				
 			}
 		}
-		void assign_table(std::string code) {
-			
-				switch (code[0]) {
-				case 'A':
-					table[0][0] = "A";
-					break;
-				case 'B':
-					table[0][0] = "B";
-					break;
-				case 'C' :
-					table[0][0] = "C";
-					break;
-				}
-				
-				switch (code[2]) {
-				case 'A' :
-					table[0][1] = "A";
-					break;
-				case 'B' :
-					table[0][1] = "B";
-					break;
-				case 'C' :
-					table[0][1] = "C";
-					break;
-				default:
-					table[0][1] = code[2];
-				}
-				switch (code[4]){
-				case 'A':
-					table[0][1] = "A";
-					break;
-				case 'B':
-					table[0][1] = "B";
-					break;
-				case 'C':
-					table[0][1] = "C";
-					break;
-					table[0][1] = code[4];
-				}
-				table[1][2] = code[3];
-			
-			
-		}
-		void int_assign(std::string code) {
-			
+		bool int_assign(std::string code) {
+			errorcount = 0;
 			if (code[6] == 'A') {
-				
 				table[0][tablecount] = "A";
 				table[1][tablecount] = code.substr(8, code.size() - 9);
 				++tablecount;
-				
-			
+				return true;
 			}
-			if (code[6] == 'B') {
+			else if (code[6] == 'B') {
 				table[0][tablecount] = "B";
-					table[1][tablecount] = code.substr(8, code.size() - 9);
-					++tablecount;
-				
-				
+				table[1][tablecount] = code.substr(8, code.size() - 9);
+				++tablecount;
+				return true;
 			}
-			if (code[6] == 'C') {
+			else if (code[6] == 'C') {
 				table[0][tablecount] = "C";
 					table[1][tablecount] = code.substr(8, code.size() - 9);
 					++tablecount;
-				
+					return true;
+			}
+			else {
+				error[errorcount] = "ASSIGNMENT ERROR OCCURED";
+				errorcount++;
+				return false;
 			}
 		}
-		void calculate(std::string code) {
-			
+		bool calculate(std::string code) {
+			errorcount = 0; std::string keep;
 			if (code[0] == 'A') {
+			keep = "A";
 				if (code[2] == 'B') {
 					for (int i = 0; i < 3; i++)
 					{
 						if (table[0][i] == "B") {
-							if (table[1][i] != "unassigned")
+							if (table[1][i] != "unassigned") {
 								temp = std::stoi(table[1][i]);
+							}
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'B' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
+
 				}
 				if (code[4] == 'B') {
-					for (int i = 0; i < 3; i++)
-					{
+					for (int i = 0; i < 3; i++) {
 						if (table[0][i] == "B") {
-							if (table[1][i] != "unassigned")
+							if (table[1][i] != "unassigned") {
 								temp1 = std::stoi(table[1][i]);
+							}
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'B' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -228,8 +196,14 @@ class Compiler
 					for (int i = 0; i < 3; i++)
 					{
 						if (table[0][i] == "C") {
-							if (table[1][i] != "unassigned")
+							if (table[1][i] != "unassigned") {
 								temp = std::stoi(table[1][i]);
+							}
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'C' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -238,20 +212,31 @@ class Compiler
 					for (int i = 0; i < 3; i++)
 					{
 						if (table[0][i] == "C") {
-							if (table[1][i] != "unassigned")
+							if (table[1][i] != "unassigned") {
 								temp1 = std::stoi(table[1][i]);
+							}
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'C' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
-			}
-			//if A ends
-			if (code[0] == 'B') {
+			}//if A ends
+			else if (code[0] == 'B') {
+			 keep = "B";
 				if (code[2] == 'A') {
 					for (int i = 0; i < 3; i++)
 					{
 						if (table[0][i] == "A") {
 							if (table[1][i] != "unassigned")
 								temp = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'A' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -261,6 +246,11 @@ class Compiler
 						if (table[0][i] == "A") {
 							if (table[1][i] != "unassigned")
 								temp1 = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'A' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -270,6 +260,11 @@ class Compiler
 						if (table[0][i] == "C") {
 							if (table[1][i] != "unassigned")
 								temp = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'C' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -279,19 +274,29 @@ class Compiler
 						if (table[0][i] == "C") {
 							if (table[1][i] != "unassigned")
 								temp1 = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'C' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
 
 			}//if B end
-
-			if (code[0] == 'C') {
+			else if (code[0] == 'C') {
+			 keep = "C";
 				if (code[2] == 'B') {
 					for (int i = 0; i < 3; i++)
 					{
 						if (table[0][i] == "B") {
 							if (table[1][i] != "unassigned")
 								temp = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'B' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -301,6 +306,11 @@ class Compiler
 						if (table[0][i] == "B") {
 							if (table[1][i] != "unassigned")
 								temp1 = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'B' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -310,6 +320,11 @@ class Compiler
 						if (table[0][i] == "A") {
 							if (table[1][i] != "unassigned")
 								temp = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'A' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
@@ -319,12 +334,16 @@ class Compiler
 						if (table[0][i] == "A") {
 							if (table[1][i] != "unassigned")
 								temp1 = std::stoi(table[1][i]);
+							else {
+								error[errorcount] = "this value has not been assigned yet : 'A' ";
+								errorcount++;
+								return false;
+							}
 						}
 					}
 				}
 
 			}//if c end
-		
 			if (code[3] == '*')
 				tempVar = temp * temp1;
 			if (code[3] == '/')
@@ -334,47 +353,102 @@ class Compiler
 			if (code[3] == '-')
 				tempVar = temp - temp1;
 			
+			
+			for (int i = 0; i < 3; i++) {
+				if(table[0][i]==keep){
+					table[1][i]=tempVar;
+					updated = true;
+					}
+			}
+			if(!updated){
+			table[0][2]=keep;
+			table[1][2]= std::to_string(tempVar);
+			}
+				
+
+			return true;
 		}
 		bool display_int(std::string code) {
+			errorcount = 0;
 			if (code[8] == 'A') {
 				for (int i = 0; i < 3; i++)
 				{
 					if (table[0][i] == "A") {
-						std::cout << table[1][i] << std::endl;
-						return true;
+						if (table[1][i] != "unassigned") {
+							std::cout << table[1][i] << std::endl;
+							return true;
+						}else {
+						error[errorcount] = "uninitialized variable used";
+						errorcount++;
+						return false;
+						}
+					}
+					else if (table[0][i] == "B");
+					else if (table[0][i] == "C");
+					else {
+						error[errorcount] = "uninitialized variable used";
+						errorcount++;
+						return false;
 					}
 				}
+				
 			}
 			else if (code[8] == 'B') {
 				for (int i = 0; i < 3; i++)
 				{
 					if (table[0][i] == "B") {
-						std::cout << table[1][i] << std::endl;
-						return true;
+						if (table[1][i] != "unassigned") {
+							std::cout << table[1][i] << std::endl;
+							return true;
+						}
+						else {
+							error[errorcount] = "uninitialized variable used";
+							errorcount++;
+							return false;
+						}
+					}
+					else if (table[0][i] == "A");
+					else if (table[0][i] == "C");
+					else{
+						error[errorcount] = "uninitialized variable used";
+						errorcount++;
+						return false;
 					}
 				}
+				
 			}
 			else if (code[8] == 'C') {
 				//std::cout << std::stoi(table[1][3]) << std::endl;
 				for (int i = 0; i < 3; i++)
 				{
 					if (table[0][i] == "C") {
-						std::cout << table[1][i] << std::endl;
-						return true;
+						if (table[1][i] != "unassigned") {
+							std::cout << table[1][i] << std::endl;
+							return true;
+						}
+						else {
+							error[errorcount] = "uninitialized variable used";
+							errorcount++;
+							return false;
+						}
 					}
+					else if (table[0][i] == "A");
+					else if (table[0][i] == "B");
+					else {
+						error[errorcount] = "uninitialized variable used";
+						errorcount++;
+						return false;
+					}
+					
 				}
-
+			
 			}
-			else
-				std::cout << "Variable not initialized" << std::endl;
-			return false;
+			
 			
 		}
 		void display_string(std::string code) {
-				std::cout<< code.substr(9, code.size() - 11);
-		}
-		
-		
+			std::cout << code.substr(9, code.size() - 11)<<std::endl;
+		}	
 	};
 
 }
